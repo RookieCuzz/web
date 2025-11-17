232be6e5a203a5cf328ae5e8f80b15f24c43e801
RookieCuzz <85008507+RookieCuzz@users.noreply.github.com>
2025-11-17 15:21:14 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['www.4399mc.cn'],
+    host: true,
+    port: 5173,
+    hmr: {
+      host: 'www.4399mc.cn'
+    }
+  }
 })
